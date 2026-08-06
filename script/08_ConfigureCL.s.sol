8424ebfc0cdd47ba535f3a90bec1a64196954a7d
LufyCZ <me@lufy.cz>
2026-08-06 12:25:40 +0000

    chore: enforce 33 percent protocol fee share

--- script/08_ConfigureCL.s.sol
+++ script/08_ConfigureCL.s.sol
@@ -7,9 +7,12 @@ import {IVault} from "../src/interfaces/IVault.sol";
 import {IProtocolFees} from "../src/interfaces/IProtocolFees.sol";
 import {IProtocolFeeController} from "../src/interfaces/IProtocolFeeController.sol";
 import {Ownable} from "../src/base/Ownable.sol";
+import {SushiSwapV4CLProtocolFeeController} from "../src/SushiSwapV4CLProtocolFeeController.sol";
 
 /// @notice Performs deployer-controlled CL setup only after all nonce 0-7 deployments exist.
 contract ConfigureCLScript is BaseScript {
+    uint256 private constant EXPECTED_PROTOCOL_FEE_SPLIT_RATIO = 33 * 1e4;
+
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address deployer = vm.addr(deployerPrivateKey);
@@ -25,6 +28,11 @@ contract ConfigureCLScript is BaseScript {
         require(clPoolManager.code.length > 0, "CL pool manager not deployed");
         require(clProtocolFeeController.code.length > 0, "CL fee controller not deployed");
         require(clPoolManagerOwner.code.length > 0, "CL pool manager owner not deployed");
+        require(
+            SushiSwapV4CLProtocolFeeController(clProtocolFeeController).protocolFeeSplitRatio()
+                == EXPECTED_PROTOCOL_FEE_SPLIT_RATIO,
+            "Unexpected protocol fee split ratio"
+        );
 
         vm.startBroadcast(deployerPrivateKey);
         IVault(vault).registerApp(clPoolManager);
